18ce5fea8dd2d5d8015bba773257e536556aaf5a
Igor Smirnov <inextgen@mail.ru>
2023-05-06 11:32:30 +0300

    PostmanEchoTest fixed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("YEST"))
+                .body("data", equalTo("TEST"))
         ;
     }
 }
